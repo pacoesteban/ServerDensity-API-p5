b644a9c0d5d5b46e860b779067d0b2bf83af7975
Paco Esteban <paco@onna.be>
2016-09-13 16:20:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+ServerDensity::API - It's new $module
+
+# SYNOPSIS
+
+    use ServerDensity::API;
+
+# DESCRIPTION
+
+ServerDensity::API is ...
+
+# LICENSE
+
+Copyright (C) Paco Esteban.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Paco Esteban <paco@onna.be>
---
+++ lib/ServerDensity/API.pm
@@ -0,0 +1,39 @@
+package ServerDensity::API;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+ServerDensity::API - It's new $module
+
+=head1 SYNOPSIS
+
+    use ServerDensity::API;
+
+=head1 DESCRIPTION
+
+ServerDensity::API is ...
+
+=head1 LICENSE
+
+Copyright (C) Paco Esteban.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Paco Esteban E<lt>paco@onna.beE<gt>
+
+=cut
+
